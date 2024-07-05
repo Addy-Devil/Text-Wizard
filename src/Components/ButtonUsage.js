6df45d984f1b91763157d135786d2f1ca251cb2a
KuhuVyas <vyaskuhu1@gmail.com>
2024-07-06 02:06:26 +0530

    Added buttons and bgcolor

--- src/Components/ButtonUsage.js
+++ src/Components/ButtonUsage.js
@@ -1,20 +1,14 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import { useState } from 'react';
 
-export default function ButtonUsage(props)
- {
-  const handleOnClick=()=>{
-    setText("Subscribed!!:)");
-    setColor("green");
-  }
-  const [text,setText]=useState("{props.content}");
-  const [color,setColor]=useState(" black");
-
-  return (<> 
-             
-<Button variant="contained" style={{backgroundColor:color}} onClick= { handleOnClick } >{text}</Button>
-</>
-  
+const ButtonUsage = ({ label, onClick }) => {
+  return (<>
+     
+         <Button onClick={onClick}>
+          {label}
+        </Button> 
+       
+     </>
   );
-}
+};
+export default ButtonUsage;
--- src/Components/TextArea.js
+++ src/Components/TextArea.js
@@ -1,28 +1,98 @@
 import * as React from 'react';
-import Box from '@mui/joy/Box';
+import { Box } from '@mui/material';
 import Textarea from '@mui/joy/Textarea';
 import IconButton from '@mui/joy/IconButton';
-
+import { useState } from 'react';
+import ButtonUsage from './ButtonUsage';
+import Typography from '@mui/material/Typography';
+import '../index.css';
 export default function TextareaRows() {
-  const [text, setText] = React.useState('hi');
+  //const [text, setText] = React.useState('hi');
 
+  
+  const [text,setText] = useState();
+  
+
+  const handleOnChange = (event)=>{
+    console.log("On change");
+    setText(event.target.value);
+  };
+ 
+  const checkIfEmpty = (action) => () => {
+    if (text === '') {
+      alert('Enter some character');
+    } else {
+      action();
+    }
+  };
+  
+   const handleUpClick = ()=>{
+     console.log("Uppercase was clicked");
+     setText(text.toUpperCase());
+   };
+   const handleLowClick = ()=>{
+    console.log("Lowercase was clicked");
+    setText(text.toLowerCase());
+  };
+  const handleClearClick = ()=>{
+    console.log("All text cleared");
+    setText('');
+  };
+  const copyText=()=>{
+    if(text){
+      navigator.clipboard.writeText(text)
+      alert("Text copied!");
+    }
+  };
+    // const handleItalicsClick = ()=>{
+    //   console.log("Uppercase was clicked");
+    //   setText(text.italics());
+    // };
+
+    // const [isBold, setBold]=useState(false);
+    
+    //   const handleBold= () =>{
+    //     setBold(!isBold)
+    //   }; const textStyle= {
+    //     fontWeight:isBold?'bold':'normal'
+    //   }
+    
   const addEmoji = (emoji) => () => setText(`${text}${emoji}`);{/*template literals*/}
   return (
     <Box sx={{ p: 5,'--Textarea-focused': 1}}>
-        EXPRESS YOURSELF...
+      <Typography variant='h4' paddingY={3}> Text-Editor </Typography> 
       <Textarea
-        placeholder="Let's dive in…"
+        placeholder="Type here"
         value={text}
-        onChange={(event) => setText(event.target.value)}
+      //onChange={(event) => setText(event.target.value)}
+      onChange={handleOnChange}
         variant="outlined"
         minRows={10}
-        startDecorator={
-            <Box sx={{ display: 'flex', gap: 0.5, flex: 1 }}>
-              <IconButton variant="outlined" color="neutral" onClick={addEmoji('👍')}>
-                👍
-              </IconButton>
-            </Box>}
-      />
+         startDecorator={
+             <Box sx={{ display: 'flex', gap: 0.5, flex: 1 }}>
+               <IconButton variant="outlined" color="neutral" onClick={addEmoji('👍')}>
+                 👍
+               </IconButton>
+             </Box>}
+      /> 
+
+    <ButtonUsage label="Uppercase" onClick={checkIfEmpty(handleUpClick)} />
+    <ButtonUsage label="Lowercase" onClick={checkIfEmpty(handleLowClick)} />
+    <ButtonUsage label="Copy Text" onClick={checkIfEmpty(copyText)} />
+    <ButtonUsage label="Clear All" onClick={checkIfEmpty(handleClearClick)} />
+    {/* <ButtonUsage label="Bold" onClick={handleBold} /> */}
+
+    <Box>
+      <Typography variant="h4" paddingY={3} >
+        Session Insights
+      </Typography>
+      {/* <Typography>
+        You've entered {(text.split(" ").length)} words and {(text.length)} characters.
+      </Typography> */}
+
     </Box>
+    </Box>
+  
+    
   );
 }
